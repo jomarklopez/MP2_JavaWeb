47a5c9db876f4dcfbf84878215b310591f59dfa7
Patrick <Patrick@192.168.1.15>
2021-02-25 20:28:09 +0800

    added dbmanager class and listener

---
+++ src/java/com/libraweb/utils/DbManager.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.libraweb.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Patrick
+ */
+public class DbManager{
+    String url;
+    String user;
+    String pass;
+    Connection c;
+    
+    public DbManager(String url, String user, String pass) throws SQLException{
+        url = this.url;
+        user = this.url;
+        pass = this.url;
+        c = DriverManager.getConnection(url, user, pass);
+        
+    }
+    
+    public Connection getConnection(){
+        return this.c;
+    }
+    
+    public void closeConnection() throws SQLException {
+        c.close();
+    }
+            
+}
